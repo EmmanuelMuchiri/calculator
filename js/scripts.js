4c2c91fafcb89f54333d968944e92180b8178660
Emmanuel Muchiri <Emmanuel.Muchiri@outlook.com>
2019-05-29 10:31:36 +0300

    added javascript to my scripts.js file

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var add=function(numberOne,numberTwo)
+{
+return numberOne+numberTwo;
+}
+var numberOne=parseInt(prompt("enter your first number"));
+var numberTwo=parseInt(prompt("enter your second number"));
+alert(add(numberOne,numberTwo));
