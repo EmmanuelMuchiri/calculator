1305871a7802749ed5b49388d4fd98cdd6663818
Emmanuel Muchiri <Emmanuel.Muchiri@outlook.com>
2019-05-29 13:02:47 +0300

    modified my script.css

--- js/scripts.js
+++ js/scripts.js
@@ -2,28 +2,28 @@ var add=function(numberOne,numberTwo)
 {
 return numberOne+numberTwo;
 }
-var numberOne=parseInt(prompt("enter your first number"));
+var numberOne=parseInt(prompt("enter your first number to perform addition"));
 var numberTwo=parseInt(prompt("enter your second number"));
-alert(add(numberOne,numberTwo));
+alert("The sum after adding"+numberOne+"and"+numberTwo+"="+(add(numberOne,numberTwo)));
 
 var subtract=function(numberOne,numberTwo)
 {
   return numberOne-numberTwo;
 }
-var numberOne=parseInt(prompt("Enter your first number"));
+var numberOne=parseInt(prompt("Enter your first number to perform subtraction"));
 var numberTwo=parseInt(prompt("Enter your second number"));
-alert(subtract(numberOne,numberTwo));
+alert("The result after subtracting"+numberOne+"and"+numberTwo+"="+(subtract(numberOne,numberTwo)));
  var multiply=function(numberOne,numberTwo)
  {
    return numberOne*numberTwo;
  }
- var numberOne=parseInt(prompt("Enter your first number"));
+ var numberOne=parseInt(prompt("Enter your first number to perform multiplication"));
  var numberTwo=parseInt(prompt("Enter your second number"));
- alert(multiply(numberOne,numberTwo));
+alert("The result after multiplying"+numberOne+"and"+numberTwo+"="+(multiply(numberOne,numberTwo)));
 var divide=function(numberOne,numberTwo)
 {
   return numberOne/numberTwo;
 }
-var numberOne=parseInt(prompt("Enter your first number"));
+var numberOne=parseInt(prompt("Enter your first number to divide with the other number"));
 var numberTwo=parseInt(prompt("Enter your second number"));
-alert(divide(numberOne,numberTwo));
+alert("The result after dividing"+numberOne+"and"+numberTwo+"="+(divide(numberOne,numberTwo)));
